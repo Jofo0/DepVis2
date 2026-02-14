6ea7c246cd91a001603e4159c9a5810e898e29f9
Jofo0 <157721468+Jofo0@users.noreply.github.com>
2026-02-14 22:19:50 +0100

    Add if is tag do not let branch history

--- Frontend/src/pages/BranchHistory/BranchHistory.tsx
+++ Frontend/src/pages/BranchHistory/BranchHistory.tsx
@@ -40,6 +40,13 @@ const History = () => {
   if (!branch || isLoading || !data)
     return <div>Loading branch history...</div>;
 
+  if (branch.isTag) {
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <Card className="flex flex-col justify-center items-center w-1/3 self-center ">
+        <div className="p-4">Branch history is not available for tags</div>
+      </Card>
+    </div>;
+  }
   if (data?.processingStep === ProcessStep.NotStarted)
     return (
       <div className="flex flex-col items-center justify-center w-full h-full">
