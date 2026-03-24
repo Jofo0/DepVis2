b8a28fc4c99854a6032140e5b9eb91d2880f0f44
Jofo0 <157721468+Jofo0@users.noreply.github.com>
2026-03-24 17:31:06 +0100

    Add migrations

---
+++ Backend/DepVis.Core/Migrations/20260324163050_AddDirectVulnsCount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DepVis.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDirectVulnsCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DirectVulnerabilityCount",
+                table: "BranchHistories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DirectVulnerabilityCount",
+                table: "BranchHistories");
+        }
+    }
+}
