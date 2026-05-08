997ef6266f9884f640fd1ab934338cb23af900db
Jozef Gajdos <jofogajdos@gmail.com>
2026-05-08 22:10:37 +0200

    Fix naming migration

---
+++ Backend/DepVis.Core/Migrations/20260508201027_FixNaming.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DepVis.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HistoryProcessinStatus",
+                table: "ProjectBranches",
+                newName: "HistoryProcessingStatus");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HistoryProcessingStatus",
+                table: "ProjectBranches",
+                newName: "HistoryProcessinStatus");
+        }
+    }
+}
